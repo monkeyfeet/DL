0c188ea0f0068d5f7ff3b4be97e6bf17318c5767
psserver <client@plasticstudio.co.nz>
2017-09-15 12:27:32 +1200

    Admin emails

--- site/code/DataObjects/FormSubmission.php
+++ site/code/DataObjects/FormSubmission.php
@@ -86,8 +86,8 @@ class FormSubmission extends DataObject {
 
 			case 'ContactPage':
 				// --- ADMIN EMAIL ---
-				if( isset($this->Origin()->AdminEmail) ){
-					$to = $this->Origin()->AdminEmail;
+				if( isset($this->Origin()->ToEmail) ){
+					$to = $this->Origin()->ToEmail;
 				}else{
 					$to = $config->SendEmailsTo_Email;
 				}
@@ -119,12 +119,12 @@ class FormSubmission extends DataObject {
 				$email->send();
 
 				// --- CUSTOMER EMAIL ---
-				$to = '"'.$data->Name.'" <'.$data->Email.'>';
-				$data->Title = 'Thanks for your message, we\'ll be in touch soon. The details you submitted are included below for your own records.';
-				$email = Email::create($from, $to, $subject, $body);
-				$email->setTemplate('Emails/FormSubmission');
-				$email->populateTemplate( ArrayData::create($data) );
-				$email->send();
+				// $to = '"'.$data->Name.'" <'.$data->Email.'>';
+				// $data->Title = 'Thanks for your message, we\'ll be in touch soon. The details you submitted are included below for your own records.';
+				// $email = Email::create($from, $to, $subject, $body);
+				// $email->setTemplate('Emails/FormSubmission');
+				// $email->populateTemplate( ArrayData::create($data) );
+				// $email->send();
 
 				break;
 
